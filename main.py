6c934833a8eaf1c33e1afbae28729472c647a185
SomeMinecraftModder <staff@webgarbage.fr>
2021-11-20 14:13:11 +0100

    Some cleaning

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ print("erreur: ton nom est maintenant jean-pierre decrochage")
 
 nomprof = ("enfoiré: math", "amé: français", "homework: histoire")
 qi = 120
-dep = 0
+depression = 0
 jour = 1
-heurcolle = 0
-ob = 0
+heure_de_colle = 0
+observation = 0
 sanc = False
 
 while 1:
@@ -21,17 +21,17 @@ while 1:
     thing = randint(1, 4)
     if thing == 1:
         b = randint(1, 5)
-        heurcolle = heurcolle + b
+        heure_de_colle = heure_de_colle + b
         print("plus %s heure de colle !" % b)
     elif thing == 2:
         print("t'a une observation !!!!")
-        ob = ob + 1
+        observation = observation + 1
     elif thing == 3:
         print("sanction ! ta mère t'attend à ta maison !!!")
         sanc = True
     else:
         print("t'a de la chance que t'a rien !!!")
-    if dep > 99:
+    if depression > 99:
         print("tu t'est suicidé !")
         break
     if qi < 1:
@@ -40,27 +40,27 @@ while 1:
     if qi > 300:
         print("t'a trop de qi ahahahha t'est mort je sais pas comment ahahhaha")
         break
-    if heurcolle > 30 or ob > 30:
+    if heure_de_colle > 30 or observation > 30:
         print("t'est mort ahhahaha t'a trop d'heure de colles/observation ahhahah")
         break
     print("---bilan---")
-    print("heure de colle: %s" % heurcolle)
-    print("observation: %s" % ob)
+    print("heure de colle: %s" % heure_de_colle)
+    print("observation: %s" % observation)
     print("---")
     print("ancien qi : %s" % qi)
-    print("ancienne depresion : %s" % dep)
+    print("ancienne depresion : %s" % depression)
     print("---")
-    d = ((dep/3) + ((int(heurcolle) / 5) + ob + (dep/35)))
+    d = ((depression / 3) + ((int(heure_de_colle) / 5) + observation + (depression / 35))) # mess
     if sanc:
         d = d + 15
         sanc = False
-    dep = int(d)
-    print("depresion: %s" % dep)
-    e = qi - (dep/25)
+    depression = int(d)
+    print("depresion: %s" % depression)
+    e = qi - (depression / 25)
     qi = int(e)
     print("qi: %s" % qi)
     truc = None
-    while 1:
+    while 1: # loop for input
         f = input("(1)travailler ou se (2)reposer ?")
         try:
             truc = int(f)
@@ -68,30 +68,32 @@ while 1:
             pass
         if truc == 1:
             if randint(0, 5):
-                dep = dep + randint(2, 15)
-            dep = dep + randint(10, 25)
-            heurcolle = heurcolle - 1
-            ob = ob - 1
+                depression = depression + randint(2, 15)
+            depression = depression + randint(10, 25)
+            heure_de_colle = heure_de_colle - 1
+            observation = observation - 1
             qi = qi + 5
-            if heurcolle < 1:
-                heurcolle = 0
-            if ob <1:
-                ob = 0
+            if heure_de_colle < 1:
+                heure_de_colle = 0
+            if observation < 1:
+                observation = 0
+                break
         elif truc == 2:
-            dep = dep - randint(8, 18)
-            if heurcolle <1:
+            depression = depression - randint(8, 18)
+            if heure_de_colle < 1:
                 heurcoll = 0
+            break
         else:
             print("FAIT QUELQCHOSE LOLOL MDR XDDDDDD")
             continue
-    if dep < 1:
-            dep = 0
+    if depression < 1:  # fix negative depression
+        depression = 0
 print("hahahah t'est mort ! bon débara !")
 if randint(0, 1):
     print("tes parents ont pleuré pour toi... il te regrette !")
 else:
     print("tes parents n'ont pas pleuré pour toi... il te regrette pas !")
-score = (dep * 100) + (ob * 100) + (qi * 150) + heurcolle + (jour*100)
+score = (depression * 100) + (observation * 100) + (qi * 150) + heure_de_colle + (jour * 100)
 print("score: %s" % score)
 while 1:
     pass
